57677cb86a8d4dd6f8701c25bea02536346c0a27
Vtech325 <malikrob263@yahoo.com>
2021-03-31 23:55:26 -0400

    Main
    Basic Menu creation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// Call Methods & Menu display
+
+        int wChoice;
+        int oChoice;
+
+        System.out.println("============================================");
+        System.out.println("Welcome to Battle Simulator! (Made by Blonk)");
+        System.out.println("============================================");
+
+        System.out.println("Choose your weapon!");
+        System.out.println("1) Sword");
+        System.out.println("2) Spear");
+        System.out.println("3) Mace");
+
+        System.out.println("Choose your opponent!");
+        System.out.println("1) Zombie");
+        System.out.println("2) Skeleton");
+        System.out.println("3) Beast");
     }
 }
